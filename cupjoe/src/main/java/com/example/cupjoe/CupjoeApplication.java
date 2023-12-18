694836834f711a3b16ee2ce7aacf3b18f8e6a750
SiddaharthaPaudel <psiddhartha62@gmail.com>
2023-12-18 10:37:23 +0545

    first commit and innitialization of project

---
+++ cupjoe/src/main/java/com/example/cupjoe/CupjoeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cupjoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupjoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupjoeApplication.class, args);
+	}
+
+}
---
+++ cupjoe/src/test/java/com/example/cupjoe/CupjoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cupjoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupjoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
